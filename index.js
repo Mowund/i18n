1b41df83a0e21105757ddf685d49bc73dda9f4e6
Smidul <45880040+Smidul@users.noreply.github.com>
2022-01-07 22:32:10 -0300

    Fix file directory

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import { readdirSync, readFileSync } from 'node:fs';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url),
+  __dirname = dirname(__filename);
 
 export function getBotStaticCatalog() {
-    const folders = readdirSync('./locale'),
-        list = {};
+  const folders = readdirSync(path.join(__dirname, 'locale')),
+    list = {};
 
-    for (const folder of folders) {
-        list[folder] = JSON.parse(readFileSync(`./locale/${folder}/bot.json`));
-    }
-    return list;
+  for (const folder of folders) {
+    list[folder] = JSON.parse(readFileSync(path.join(__dirname, `locale/${folder}/bot.json`)));
+  }
+  return list;
 }
